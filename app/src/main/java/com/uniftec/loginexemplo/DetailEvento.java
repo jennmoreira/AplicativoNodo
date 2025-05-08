35202341f0a72fca9475db66444703f468e8dcf1
jennmoreira <163209976+jennmoreira@users.noreply.github.com>
2025-05-08 11:20:09 -0300

    Add files via upload

---
+++ app/src/main/java/com/uniftec/loginexemplo/DetailEvento.java
@@ -0,0 +1,41 @@
+package com.example.tablayout;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.tablayout.databinding.ActivityDetailEventoBinding;
+
+public class DetailEvento<DetailEventoBinding> extends AppCompatActivity {
+    DetailEvento binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityDetailEventoBinding binding = DetailEvento.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Intent intent = this.getIntent();
+        if (intent != null){
+            String name = intent.getStringExtra("name");
+            String time = intent.getStringExtra("time");
+            int ingredients = intent.getIntExtra("ingredients", R.string.maggiIngredients);
+            int desc = intent.getIntExtra("desc", R.string.maggieDesc);
+            int image = intent.getIntExtra("image", R.drawable.maggi);
+            binding.detailName.setText(name);
+            binding.detailTime.setText(time);
+            binding.detailDesc.setText(desc);
+            binding.detailIngredients.setText(ingredients);
+            binding.detailImage.setImageResource(image);
+        }
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/uniftec/loginexemplo/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.tablayout;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabItem;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        tabItem = findViewById(R.id.tabItem);
+        viewPager = findViewById(R.id.viewpager);
+
+        tabItem.setupWithViewPager(viewPager);
+
+        VPAdapter vpAdapter = new VPAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new FragmentEventos(),"Eventos");
+        vpAdapter.addFragment(new FragmentPrestadores(),"Candidatos");
+        viewPager.setAdapter(vpAdapter);
+
+
+
+
+
+    }
+}
